212c5425c0aec24f520a2572586e9fc807422fe1
Evgenii <evtatdevelop@gmail.com>
2021-03-18 20:05:41 +0700

    Add html, styles,  for experiance && education

--- js/app.js
+++ js/app.js
@@ -6,15 +6,24 @@ const app = doc.querySelector('#app');
 /**
  * Skill Progress
  */
+function autoCounter(element, current, level) {
+  element.innerHTML = ++current;
+  if (current < level) {
+    setTimeout(() => autoCounter(element, current, level), 1000 / level);
+  }
+}
 function showSkillProgress() {
   const winHeight = document.documentElement.clientHeight;
   const skillListLis = doc.querySelectorAll('.skills-list>li');
   skillListLis.forEach(skill => {
     const level = skill.dataset.level;
-    skill.querySelector('.levelNum').textContent = level;
+    skill.querySelector('.levelNum').innerHTML = level;
     const progress = skill.querySelector('.progress')
     if (progress.getBoundingClientRect().bottom < winHeight) {
       progress.style.width = `${level}%`;
+      // if(skill.querySelector('.levelNum').textContent < level) {
+      //   autoCounter(skill.querySelector('.levelNum'), 0, level);
+      // }
     }
   })
 }
