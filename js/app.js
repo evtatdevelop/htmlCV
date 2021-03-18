d88d43bb338d027a5b9499f71273685e9f2755cb
Evgenii <evtatdevelop@gmail.com>
2021-03-18 14:30:57 +0700

    Add html, styles, js  for SKILLS

--- js/app.js
+++ js/app.js
@@ -2,3 +2,21 @@
 
 const doc = document;
 const app = doc.querySelector('#app');
+
+/**
+ * Skill Progress
+ */
+function showSkillProgress() {
+  const winHeight = document.documentElement.clientHeight;
+  const skillListLis = doc.querySelectorAll('.skills-list>li');
+  skillListLis.forEach(skill => {
+    const level = skill.dataset.level;
+    skill.querySelector('.levelNum').textContent = level;
+    const progress = skill.querySelector('.progress')
+    if (progress.getBoundingClientRect().bottom < winHeight) {
+      progress.style.width = `${level}%`;
+    }
+  })
+}
+showSkillProgress();
+window.addEventListener('scroll', showSkillProgress);
